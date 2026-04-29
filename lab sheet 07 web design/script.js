0c7cb7daa69abdc3012f890a37d44fbdc2fe5190
shivanshukashyap284-droid <shivanshukashyap284@gmail.com>
2026-04-29 10:20:54 +0530

    Add files via upload

---
+++ lab sheet 07 web design/script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    let menu = document.getElementById("menu");
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+}
+
+function showMessage() {
+    alert("Thank you for visiting!");
+}
